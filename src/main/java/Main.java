e488a2275d5a89dcc00f1b99f4fd14b39808a0a9
Regurus <weltall2004@gmail.com>
2018-10-19 03:07:22 +0300

    initial

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,12 @@
+import Model.Settings;
+
 public class Main {
     public static void main(String[] args) {
         //deployment check
         //if not deployed do deployment procedure
         //initialize database connections
         //open Login UI
+        Settings.getInstance().setProperty("key","value");
+        Settings.getInstance().saveFile();
     }
 }

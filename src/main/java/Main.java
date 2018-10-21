ffb5439f96ce3ad0c5c989235202cab02c1be8cf
Regurus <weltall2004@gmail.com>
2018-10-21 22:10:11 +0300

    first UI

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,24 @@
 import Model.Settings;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
-public class Main {
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("src/main/java/View/signIn.fxml"));
+        //root.getStylesheets().add(getClass().getResource("Main_Style.css").toString());
+        primaryStage.setTitle("Nituts");
+        primaryStage.setScene(new Scene(root, 1000, 1000));
+        //SetStageCloseEvent(primaryStage);
+        primaryStage.initStyle(StageStyle.TRANSPARENT);
+        primaryStage.show();
+    }
+
     public static void main(String[] args) {
         //deployment check
         //if not deployed do deployment procedure

4122c9f4af7acd93bb0830c2eaaa6e75be27bf38
Regurus <weltall2004@gmail.com>
2018-12-07 17:27:32 +0200

    main prototype UI added - refined to vector icons

---
+++ src/main/java/View/ResultItemController.java
@@ -0,0 +1,11 @@
+package View;
+
+import javafx.fxml.FXML;
+
+public class ResultItemController {
+
+    @FXML
+    public void openItem(){
+
+    }
+}
---
+++ src/main/java/View/UiController.java
@@ -0,0 +1,28 @@
+package View;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
+
+
+public class UiController implements Initializable {
+
+    @FXML
+    private HBox parent;
+
+    private double xOffset = 0;
+    private double yOffset = 0;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+
+
+
+
+}

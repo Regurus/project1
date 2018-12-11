e16e9cdf8f51f75d580f2c2d92aa1588972389b3
razsh98 <razsh98@gmail.com>
2018-12-11 21:23:51 +0200

    this is a null commit

--- src/main/java/Controller/HotDealsInterface.java
+++
@@ -1,9 +0,0 @@
-package Controller;
-
-/**
- * This class contains an interface to help display the hot deals immediately when a customer logs in.
- * //TODO define hot deal policy. right now displays 20 random vacations from the Vacation Database.
- */
-public class HotDealsInterface extends VacationInteraction{
-
-}
